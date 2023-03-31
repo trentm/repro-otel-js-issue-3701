e41939e2845a804e37fb2a8f172bc33006097672
Trent Mick <trentm@gmail.com>
2023-03-31 12:12:02 -0700

    attempt with the setup code from the issue

--- src/repro.ts
+++ src/repro.ts
@@ -1,14 +1,78 @@
-import { NodeSDK } from '@opentelemetry/sdk-node'
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base'
+import os from "node:os";
+// import { Logger } from "@warrify/json-logger";
+import {
+  diag,
+  DiagLogLevel,
+  trace,
+} from "@opentelemetry/api";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { AwsLambdaInstrumentation } from "@opentelemetry/instrumentation-aws-lambda";
+import { DnsInstrumentation } from "@opentelemetry/instrumentation-dns";
+import { AwsInstrumentation } from "@opentelemetry/instrumentation-aws-sdk";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
+import {
+  SemanticAttributes,
+  SemanticResourceAttributes,
+  CloudProviderValues,
+  CloudPlatformValues,
+} from "@opentelemetry/semantic-conventions";
 
-const sdk = new NodeSDK({
-    traceExporter: new ConsoleSpanExporter(),
-    instrumentations: [getNodeAutoInstrumentations()],
-});
+// const logger = new Logger("tracing");
 
-sdk.start();
+// diag.setLogger(logger as any, DiagLogLevel.ALL);
 
-setInterval(() => {
-    console.log('hi')
-}, 5000)
+try {
+  const provider = new NodeTracerProvider();
+  const spanProcessor = new BatchSpanProcessor(
+    new OTLPTraceExporter({
+      url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+    })
+  );
+
+  provider.addSpanProcessor(spanProcessor);
+  provider.register();
+  trace.setGlobalTracerProvider(provider);
+
+  registerInstrumentations({
+    instrumentations: [
+      new AwsInstrumentation({}),
+      new AwsLambdaInstrumentation({
+        requestHook: (span, { event, context }) => {
+          span.setAttributes({
+            [SemanticAttributes.AWS_LAMBDA_INVOKED_ARN]:
+              context.invokedFunctionArn,
+          });
+        },
+        responseHook: (span, { err, res }) => {
+          if (err instanceof Error) {
+            span.recordException(err);
+          }
+          if (res) {
+            span.setAttribute("faas.res", res);
+          }
+        },
+      }),
+      new DnsInstrumentation({}),
+      new HttpInstrumentation(),
+    ],
+  });
+} catch (err: any) {
+  console.error(err);
+}
+
+import { createServer } from 'node:http'
+const server = createServer((req, res) => {
+    req.resume()
+    req.on('end', () => {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        })
+        res.end('pong')
+    })
+})
+server.listen(3000, () => {
+    console.log('listening at http://127.0.0.1:3000')
+})
